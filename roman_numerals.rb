ad5457feae62b46f2600f8c0dc9170e29c85f8f6
Mandy Chen <>
2021-06-09 12:40:42 -0700

    type + add hash version

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,3 +1,6 @@
-number_map = [nil, 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VII', 'IX', 'X']
+number_map = [nil, 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X']
+
+number_map_2 = { 1 => 'I', 2 => 'II', 3 => 'III', 4 => 'IV', 5 => 'V', 6 => 'VI', 7 => 'VII', 8 => 'VIII', 9 => 'IX', 10 => 'X'}
 
 puts number_map[9]
+puts number_map_2[4]
