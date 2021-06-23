35e4ade870e6ab396007edb4ffd890588e2350e2
Mandy <mandychen.art@gmail.comm>
2021-06-22 23:43:36 -0700

    problem statement for blanket patterns challenge

---
+++ blanket_patterns.rb
@@ -0,0 +1,9 @@
+colors = "RRGGBBYYKK"
+# output 20 rows
+# on each row, shift the characters to the left by 
+  # removing the first character and putting it at the end
+# first iteration: "RGGBBYYKKR"
+
+# use loops or iterators
+  # string[0], string[1..-1]
+  # split, shift, join after converting to array
