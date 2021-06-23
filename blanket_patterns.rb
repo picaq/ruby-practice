0e6e497af244fd9a6492bcef973fc97ea8b2f9a5
Mandy <mandychen.art@gmail.comm>
2021-06-22 23:58:51 -0700

    add string and array method solutions

--- blanket_patterns.rb
+++ blanket_patterns.rb
@@ -1,4 +1,5 @@
 colors = "RRGGBBYYKK"
+colours = "//--**~~\\\\"
 # output 20 rows
 # on each row, shift the characters to the left by 
   # removing the first character and putting it at the end
@@ -7,3 +8,15 @@ colors = "RRGGBBYYKK"
 # use loops or iterators
   # string[0], string[1..-1]
   # split, shift, join after converting to array
+
+# colorArr = colors.split
+
+20.times do
+  puts colors
+  colors = colors[1..-1] + colors[0]
+end
+
+20.times do
+  puts colours
+  colours = (colours.split('')[1..-1] << colours.split('').shift).join
+end
