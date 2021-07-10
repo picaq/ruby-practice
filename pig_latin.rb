70d45148f4dfbd9099c9951a07fe3220e7f7e063
Mandy Chen <>
2021-07-09 19:37:33 -0700

    start pig_latin challenge

---
+++ pig_latin.rb
@@ -0,0 +1,17 @@
+# begin with 1 consonant: move consonant to end & add "ay"
+  # pig => igpay
+  # latin => atinlay
+
+# begin with vowel: add "ay"
+  # elevator => elevatoray
+
+# begin with consonant cluster: all consonants move to end & add "ay"
+  # glove => oveglay
+  # where => erewhay
+
+def pig_latin(word)
+  # ...
+end
+
+puts pig_latin("Ruby")
+# Ubyray
