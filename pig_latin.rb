436b5110af78ee00bb08acbfc568c4f0e5925612
Mandy Chen <>
2021-07-09 21:08:27 -0700

    complete pig latin challenge with caps sensitivity

--- pig_latin.rb
+++ pig_latin.rb
@@ -12,13 +12,33 @@
 VOWELS = ['a', 'e', 'i', 'o', 'u']
 
 def pig_latin(word)
+  caps = false
+  if word[0] == word[0].upcase
+    caps = true
+  end
   # convert word to array
+  word_arr = word.downcase.split('')
   # find first vowel position
+  last = word_arr.length - 1
+  0.upto(last) do |i|
+    if VOWELS.include?(word_arr[i])
+      case i <=> 0
+      when 0 ; return caps ? (word + 'ay').capitalize : word + 'ay'
+      when 1 ; return caps ? (word[i..last] + word[0..i-1] + 'ay').capitalize : word[i..last] + word[0..i-1] + 'ay'
+      end
+    end
+  end
   #   Array#find_index
   # if pos > 0
   # remove characters up to position
   # return main + front + 'ay'
 end
+puts pig_latin("Ruby") # Ubyray
+puts pig_latin("pig")
+puts pig_latin("latin")
+puts pig_latin("elevator")
+puts pig_latin("glove")
+puts pig_latin("where")
+puts pig_latin("racecar")
+puts pig_latin("Space")
 
-puts pig_latin("Ruby")
-# Ubyray
