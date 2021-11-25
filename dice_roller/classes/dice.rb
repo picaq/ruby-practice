6a99375d3f1d69627533cb2db7d9fe00b0734e91
Mandy Chen <34908590+picaq@users.noreply.github.com>
2021-11-24 16:59:33 -0800

    dice roller challenge

---
+++ dice_roller/classes/dice.rb
@@ -0,0 +1,37 @@
+# DiceSet instance contains 2 Dice instances
+# DiceSet#roll rolls both dice
+# rand(6)
+# DiceSet#display display the values of both dice
+#
+#
+
+class Dice
+  def initialize
+    @value = rand(6) + 1
+  end
+
+  def roll
+    @value = rand(6) + 1
+  end
+
+  def value
+    @value
+  end
+
+end
+
+class DiceSet
+  def initialize
+    @d1 = Dice.new
+    @d2 = Dice.new
+  end
+
+  def display
+    "[ #{@d1.value} ] - [ #{@d2.value} ]"
+  end
+
+  def roll
+    [@d1.roll, @d2.roll]
+  end
+
+end
